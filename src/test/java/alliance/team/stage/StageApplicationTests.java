521da9b60955025c6209a0865baa2fd25a4d36e7
Cheikhnd11 <cn7061611@gmail.com>
2024-11-15 19:47:04 +0000

    Initialisation du projet

---
+++ src/test/java/alliance/team/stage/StageApplicationTests.java
@@ -0,0 +1,13 @@
+package alliance.team.stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
